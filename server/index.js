dca6705671a75f4c7c367509fd4b34433c4a1540
devayanm <devayan9689@gmail.com>
2025-10-06 13:43:28 +0530

    NexSuite initialized

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const homeRoutes = require("./routes/statRoutes");
+const emailRoutes = require("./routes/emailRoutes");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const contactListRoutes = require("./routes/contactListRoutes");
+const imageRoutes = require("./routes/imageRoutes");
+const { loadAndScheduleActiveEmails } = require("./services/cronJobScheduler");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const dbURL = process.env.DB_URL;
+
+require("dotenv").config();
+
+const app = express();
+const DB_URI = dbURL;
+app.use(express.json());
+app.use(cookieParser(process.env.SECRET));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.SITE_URL,
+      "http://localhost:5174",
+      "http://localhost:5175",
+    ],
+    credentials: true,
+  })
+);
+app.use("/uploads", express.static("uploads"));
+app.use(fileUpload());
+
+mongoose
+  // .connect(process.env.DB_URI)
+  .connect(DB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+loadAndScheduleActiveEmails();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/home", homeRoutes);
+app.use("/api/emails", emailRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/contact-lists", contactListRoutes);
+app.use("/api", imageRoutes);
+
+
+//temporary port changed from 4040 to 4000
+const PORT = process.env.PORT || 4040;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
